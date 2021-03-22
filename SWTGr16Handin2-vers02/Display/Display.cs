04cd5bfe27db3db558a53b198220d857a3ed9b85
MinheeDK <minheedk@hotmail.com>
2021-03-22 19:13:39 +0100

    Tilføjet Display og tilhørende interface

---
+++ SWTGr16Handin2-vers02/Display/Display.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SWTGr16Handin2_vers02.Display
+{
+    public class Display
+    {
+        public void PrintConnectDevice()
+        {
+            Console.WriteLine("Tilslut mobiltelefon");
+        }
+
+        public void PrintScanRfid()
+        {
+            Console.WriteLine("Scan RFID");
+        }
+
+        public void PrintConnectionError()
+        {
+            Console.WriteLine("Fejl ved tilslutning \r\n Prøv at tilslutte mobiltelefonen igen");
+        }
+
+        public void PrintBusy()
+        {
+            Console.WriteLine("Skabet er optaget");
+        }
+
+        public void PrintRfidFail()
+        {
+            Console.WriteLine("Fejl ved scanning af RFID \r\nPrøv igen");
+        }
+
+        public void PrintRemoveDevice()
+        {
+            Console.WriteLine("Tag mobiltelefonen");
+        }
+
+        public void PrintChargingOn()
+        {
+            Console.WriteLine("Opladningen er i gang");
+        }
+
+        public void PrintFullyCharged()
+        {
+            Console.WriteLine("Fuldt opladt");
+        }
+    }
+}
---
+++ SWTGr16Handin2-vers02/Display/IDisplay.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SWTGr16Handin2_vers02.Display
+{
+    public interface IDisplay
+    {
+        public void PrintConnectDevice();
+
+        public void PrintScanRfid();
+
+        public void PrintConnectionError();
+
+        public void PrintBusy();
+
+        public void PrintRfidFail();
+
+        public void PrintRemoveDevice();
+
+        public void PrintChargingOn();
+
+        public void PrintFullyCharged();
+    }
+}
